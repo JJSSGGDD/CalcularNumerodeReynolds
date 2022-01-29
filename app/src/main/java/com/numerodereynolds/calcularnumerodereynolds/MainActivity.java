bfc9cf363a4f75e7fa2b245cb4705102a29b8c75
JJSSGGDD <92124067+JJSSGGDD@users.noreply.github.com>
2022-01-29 13:29:04 -0500

    FIRST TEST SPINNER

--- app/src/main/java/com/numerodereynolds/calcularnumerodereynolds/MainActivity.java
+++ app/src/main/java/com/numerodereynolds/calcularnumerodereynolds/MainActivity.java
@@ -3,15 +3,20 @@ package com.numerodereynolds.calcularnumerodereynolds;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private EditText densidaddelfluido, diametrodelatuberia, velocidaddelfluido, velocidaddinamica;
     private Button calcularnumerodereynolds, eliminardatos;
     private TextView numerodereynolds, tipodeflujo;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         eliminardatos = findViewById(R.id.btneliminardatos);
         numerodereynolds = findViewById(R.id.txtnumerodereynolds);
         tipodeflujo = findViewById(R.id.txttipodeflujo);
+        spinner = findViewById(R.id.spinner1);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.diametrotuberia, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     //haciendo las operaciones
@@ -35,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         double ndiametrodelatuberia = Double.parseDouble(diametrodelatuberia.getText().toString());
         double nvelocidaddelfluido = Double.parseDouble(velocidaddelfluido.getText().toString());
         double nvelocidaddinamica = Double.parseDouble(velocidaddinamica.getText().toString());
+
+        if (diametrotuberia =[ft])
+            ;
         double resultadonumerodereynolds = ((ndensidaddelfluido * ndiametrodelatuberia * nvelocidaddelfluido) / (nvelocidaddinamica));
 
         //mostrar el resultado
@@ -55,5 +69,17 @@ public class MainActivity extends AppCompatActivity {
         velocidaddinamica.setText("");
         numerodereynolds.setText("  Numero de Reynolds :");
         tipodeflujo.setText("  Tipo de Flujo :");
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_LONG).show();;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
