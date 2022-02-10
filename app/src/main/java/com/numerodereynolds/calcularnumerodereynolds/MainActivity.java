ddb25108d105570fd20461cdc1e44bfdd3b15c0a
JJSSGGDD <92124067+JJSSGGDD@users.noreply.github.com>
2022-02-10 08:38:31 -0500

    SPINNERS DONE

--- app/src/main/java/com/numerodereynolds/calcularnumerodereynolds/MainActivity.java
+++ app/src/main/java/com/numerodereynolds/calcularnumerodereynolds/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView numerodereynolds, tipodeflujo;
     private Spinner spinner_densidaddelfluido, spinner_diametrodelatuberia, spinner_velocidaddelfluido, spinner_viscosidaddinamica;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,22 +38,21 @@ public class MainActivity extends AppCompatActivity {
         spinner_velocidaddelfluido = (Spinner) findViewById(R.id.spinner3);
         spinner_viscosidaddinamica = (Spinner) findViewById(R.id.spinner4);
 
-        String[] spinner_1 = {"[kg/m^3]", "[lbm/ft^3]", "[lbm/in^3]", };//Arreglo unidimensional
+        String[] spinner_1 = {"[kg/m^3]", "[lbm/ft^3]", "[lbm/in^3]"};//Spinner para la densidad del fluido
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinner_1 ); //sirve para poder comunicar el arreglo con la parte grafica, los dos tipos de spinner cambian muy poco, solo en el espacio
         spinner_densidaddelfluido.setAdapter(adapter);
 
-        String[] spinner_2 = {"[centipoise]", "[Pa-s]", "[lbm/s-ft]", };//Arreglo unidimensional
+        String[] spinner_2 = {"[m]", "[cm]", "[ft]", "[in]"};//Spinner para el diametro de la tuberia
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinner_2 ); //sirve para poder comunicar el arreglo con la parte grafica, los dos tipos de spinner cambian muy poco, solo en el espacio
         spinner_diametrodelatuberia.setAdapter(adapter1);
 
-        String[] spinner_3 = {"[m]", "[cm]", "[ft]", "[in]"};//Arreglo unidimensional
+        String[] spinner_3 = {"[m/s]", "[cm/s]", "[ft/s]","[in/s]"};//Spinner para la velocidad del fluido
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinner_3 ); //sirve para poder comunicar el arreglo con la parte grafica, los dos tipos de spinner cambian muy poco, solo en el espacio
         spinner_velocidaddelfluido.setAdapter(adapter2);
 
-        String[] spinner_4 = {"[m/s]", "[cm/s]", "[ft/s]","[in/s]" };//Arreglo unidimensional
+        String[] spinner_4 = {"[kg/m-s]", "[centipoise]","[Pa-s]", "[lbm/s-ft]"};//Spinner para la viscosidad dinamica
         ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinner_4 ); //sirve para poder comunicar el arreglo con la parte grafica, los dos tipos de spinner cambian muy poco, solo en el espacio
         spinner_viscosidaddinamica.setAdapter(adapter3);
-
     }
 
     //haciendo las operaciones
@@ -77,17 +75,47 @@ public class MainActivity extends AppCompatActivity {
             double ndensidaddelfluido = Double.parseDouble(densidaddelfluido.getText().toString());
             double ndiametrodelatuberia = Double.parseDouble(diametrodelatuberia.getText().toString());
             double nvelocidaddelfluido = Double.parseDouble(velocidaddelfluido.getText().toString());
-            double nvelocidaddinamica = Double.parseDouble(viscosidaddinamica.getText().toString());
+            double nviscosidaddinamica = Double.parseDouble(viscosidaddinamica.getText().toString());
 
-            if (nvelocidaddinamica == 0) {
+            if (nviscosidaddinamica == 0) {
                 Toast.makeText(this, "La viscosidad dinamica debe ser diferente de cero", Toast.LENGTH_SHORT).show();
             } else {
 
+                //Calculos para el spinner de la densidad del fluido
                 if (seleccion1.equals("[lbm/ft^3]")) {
                     ndensidaddelfluido = ndensidaddelfluido * 16.0185;
+                } if (seleccion1.equals("[lbm/in^3]")) {
+                    ndensidaddelfluido = ndensidaddelfluido / 0.000036;
                 }
 
-                double resultadonumerodereynolds = ((ndensidaddelfluido * ndiametrodelatuberia * nvelocidaddelfluido) / (nvelocidaddinamica));
+                //Calculos para el spinner del diametro de la tuberia
+                if (seleccion2.equals("[cm]")) {
+                    ndiametrodelatuberia = ndiametrodelatuberia / 0.01;
+                } if (seleccion2.equals("[ft]")) {
+                    ndiametrodelatuberia = ndiametrodelatuberia / 3.28084;
+                } if (seleccion2.equals("[in]")) {
+                    ndiametrodelatuberia = ndiametrodelatuberia / 39.3701;
+                }
+
+                //Calculos para el spinner de la velocidad del fluido
+                if (seleccion3.equals("[cm/s]")) {
+                    nvelocidaddelfluido = nvelocidaddelfluido / 100;
+                } if (seleccion3.equals("[ft/s]")) {
+                    nvelocidaddelfluido = nvelocidaddelfluido / 3.28084;
+                } if (seleccion3.equals("[in/s]")) {
+                    nvelocidaddelfluido = nvelocidaddelfluido / 39.3701;
+                }
+
+                //Calculos para el spinner de la viscosidad dinamica
+                if (seleccion4.equals("[centipoise]")) {
+                    nviscosidaddinamica = nviscosidaddinamica * 0.001;
+                //} if (seleccion4.equals("[Pa-s]")) {
+                //    nviscosidaddinamica = nviscosidaddinamica;
+                } if (seleccion4.equals("[lbm/s-ft]")) {
+                    nviscosidaddinamica = nviscosidaddinamica * 1.48816;
+                }
+
+                double resultadonumerodereynolds = ((ndensidaddelfluido * ndiametrodelatuberia * nvelocidaddelfluido) / (nviscosidaddinamica));
 
                 //mostrar el resultado
                 numerodereynolds.setText("  Numero de Reynolds : " + String.format("%.2f", resultadonumerodereynolds)); //"%2.f" = para solo dos decimales
